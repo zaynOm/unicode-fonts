7fc6d0751a9715a9d1fe501165940c8a116be23f
zaynom <ouaziz0626@gmail.com>
2024-12-26 01:51:26 +0100

    fix: use aLower parameter for lowercase letter mapping

--- src/buildFont.ts
+++ src/buildFont.ts
@@ -4,21 +4,20 @@ export function buildFont(
   digit0: number | number[],
   missing: string[],
   name: string,
-  description = ''
+  description = ""
 ) {
-  const font: number[][] = []
+  const font: number[][] = [];
   for (let i = 65; i < 91; i++) {
-    const value = typeof aMayus === 'number' ? aMayus + i - 65 : aMayus[i - 65]
-    font.push([i, value])
+    const value = typeof aMayus === "number" ? aMayus + i - 65 : aMayus[i - 65];
+    font.push([i, value]);
   }
   for (let i = 97; i < 123; i++) {
-    const value = typeof aMayus === 'number' ? aMayus + i - 97 : aMayus[i - 97]
-    font.push([i, value])
+    const value = typeof aLower === "number" ? aLower + i - 97 : aLower[i - 97];
+    font.push([i, value]);
   }
   for (let i = 48; i < 58; i++) {
-    const value = typeof digit0 === 'number' ? digit0 + i - 48 : digit0[i - 48]
-    font.push([i, value])
+    const value = typeof digit0 === "number" ? digit0 + i - 48 : digit0[i - 48];
+    font.push([i, value]);
   }
-  digit0
-  return { chars: font, missing: missing, name, description }
+  return { chars: font, missing: missing, name, description };
 }
